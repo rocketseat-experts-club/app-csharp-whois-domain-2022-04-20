b478cf7ffe3a281a102c5f7ff7970b9fb824cfcd
Sergio Cabral <git@sergiocabral.com>
2022-04-21 00:27:16 -0300

    DI: Injeção de dependência do .NET
    Está mais para um design pattern (padrão de projeto) de sistemas.
    
    DI = Dependency Injection (Injeção de Dependência)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WhoisDomain.Application;
 using WhoisDomain.Service;
 
-ITldWhoisServerDatabase tldWhoisServerDatabase = new TldWhoisServerDatabaseHardCoded();
-IWhoisQuery whoisQuery = new WhoisQueryTcp(tldWhoisServerDatabase);
-IUserInput userInput = new UserInputFromCommandLineArgument();
-IApplication application = new Whois(whoisQuery, userInput);
+using var host = Host
+    .CreateDefaultBuilder()
+    .ConfigureServices((_, services) => services
+        .AddTransient<IApplication, Whois>()
+        .AddTransient<ITldWhoisServerDatabase, TldWhoisServerDatabaseHardCoded>()
+        .AddTransient<IUserInput, UserInputFromCommandLineArgument>()
+        .AddTransient<IWhoisQuery, WhoisQueryTcp>()
+    ).Build();
 
+var application = host.Services.GetRequiredService<IApplication>();
 application.Run();
