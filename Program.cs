054d11dab1c6763952a3158c7335c1ed27a445cb
Sergio Cabral <git@sergiocabral.com>
2022-04-20 23:55:57 -0300

    SRP: Isolado serviço WhoisQueryTcp
    Aplicando o 1º princípio do S.O.L.I.D.
    
    SRP = Single Responsiblity Principle (Princípio da responsabilidade única)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
-﻿using System.Net.Sockets;
-using WhoisDomain.Service;
+﻿using WhoisDomain.Service;
 
 Console.WriteLine("Whois Internet Domain");
 
-var tldWhoisServerDatabase = new TldWhoisServerDatabaseHardCoded();
+var whoisQuery = new WhoisQueryTcp();
 var userInput = new UserInputFromCommandLineArgument();
 
 if (userInput.Domains.Length > 0)
@@ -11,30 +10,7 @@ if (userInput.Domains.Length > 0)
     foreach (var internetDomain in userInput.Domains)
     {
         Console.WriteLine($"###[ {internetDomain} ]###".PadRight(80, '#'));
-
-        var tldWhois = tldWhoisServerDatabase.GetByTld(internetDomain.Tld);
-        if (tldWhois != null &&
-            !string.IsNullOrWhiteSpace(tldWhois.Server) &&
-            tldWhois.Port > 0)
-        {
-            using var stream = new TcpClient(tldWhois.Server,tldWhois.Port).GetStream();
-            using var buffered = new BufferedStream(stream);
-
-            var writer = new StreamWriter(buffered);
-            writer.WriteLine(internetDomain);
-            writer.Flush();
-
-            var reader = new StreamReader(buffered);
-            string? response;
-            while ((response = reader.ReadLine()) != null)
-            {
-                Console.WriteLine(response);
-            }
-        }
-        else
-        {
-            Console.WriteLine($"TLD indisponível para consulta: {internetDomain.Tld}");
-        }
+        Console.WriteLine(whoisQuery.Request(internetDomain) ?? $"TLD indisponível para consulta: {internetDomain.Tld}");
     }
 
     Console.WriteLine(string.Empty.PadRight(80, '#'));
---
+++ Service/WhoisQueryTcp.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using WhoisDomain.Model;
+
+namespace WhoisDomain.Service;
+
+public class WhoisQueryTcp
+{
+    private readonly TldWhoisServerDatabaseHardCoded _tldWhoisServerDatabase;
+
+    public WhoisQueryTcp()
+    {
+        _tldWhoisServerDatabase = new TldWhoisServerDatabaseHardCoded();
+    }
+
+    public string? Request(InternetDomainModel internetDomain)
+    {
+        var tldWhoisServer = _tldWhoisServerDatabase.GetByTld(internetDomain.Tld);
+
+        if (tldWhoisServer == null || 
+            string.IsNullOrWhiteSpace(tldWhoisServer.Server) || 
+            tldWhoisServer.Port <= 0)
+        {
+            return null;
+        }
+
+        using var stream = new TcpClient(tldWhoisServer.Server, tldWhoisServer.Port).GetStream();
+        using var buffered = new BufferedStream(stream);
+        
+        var writer = new StreamWriter(buffered);
+        writer.WriteLine(internetDomain);
+        writer.Flush();
+
+        var result = new StringBuilder();
+
+        var reader = new StreamReader(buffered);
+        string? response;
+        while ((response = reader.ReadLine()) != null)
+        {
+            result.AppendLine(response);
+        }
+
+        return result.ToString();
+    }
+}
